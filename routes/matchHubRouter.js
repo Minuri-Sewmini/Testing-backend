cad8757878c5cff6be15ea60400b2ca91a0c862c
Minuri Sewmini <minurisewmini11@gmail.com>
2026-03-17 21:48:43 +0530

    remove comments

--- routes/matchHubRouter.js
+++ routes/matchHubRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
-// getAllMatches controller එකත් මෙතනට import කරගන්න
+
 import { findAndCreateMatch, getAllMatches } from "../controllers/matchHubController.js";
 
 const matchRouter = express.Router();
 
-// 1. Algorithm එක run කරලා match එකක් හොයන route එක (POST)
+
 matchRouter.post("/check", findAndCreateMatch);
 
-// 2. දැනට සිද්ධ වෙලා තියෙන සියලුම matches (History) බලන route එක (GET)
+
 matchRouter.get("/history", getAllMatches);
 
 export default matchRouter;
